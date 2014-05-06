115504a1ab60b06788c150adb941bf8e050ec851
thomasdesmoulin <thomas.desmoulin@laposte.net>
2014-05-06 19:34:44 +0200

    upgrade function

--- libs/staff.js
+++ libs/staff.js
@@ -11,29 +11,28 @@ Staff.findNearestPoint = function (tabPoints, refPoint){
       nearestPoint  = new utils.point(0,0);
 
   tabPoints.forEach(function (point, pi){
-    if (pi===0){
-      minDist=Math.sqrt(Math.pow(refPoint.x-point.x, 2)+Math.pow(refPoint.y-point.y, 2));
-      nearestPoint=point;
-    }
-    else if (Math.sqrt(Math.pow(refPoint.x-point.x, 2)+Math.pow(refPoint.y-point.y, 2))<=minDist){
-      minDist=Math.sqrt(Math.pow(refPoint.x-point.x, 2)+Math.pow(refPoint.y-point.y, 2));
-      nearestPoint=point;
-    }
+
+    var distCur  = Math.sqrt(Math.pow(refPoint.x-point.x, 2)+Math.pow(refPoint.y-point.y, 2);
+
+    minDist      = distCur <= minDist ? distCur : (pi == 0 ? distCur : minDist);
+    nearestPoint = distCur <= minDist ? point;
+    
   });
   return nearestPoint;
 }
 
 
 Staff.getCenter = function (tabPoints) {
+
   var min     = new utils.point(0,0),
       max     = new utils.point(0,0),
       center  = new utils.point(0,0);
 
   tabPoints.forEach(function (point, pi){
-    min.x=point.x<min.x ? point.x : (pi === 0 ? point.x : min.x);
-    min.y=point.y<min.y ? point.y : (pi === 0 ? point.y : min.y);
-    max.x=point.x>max.x ? point.x : (pi === 0 ? point.x : max.x);
-    max.y=point.y>max.y ? point.y : (pi === 0 ? point.y : max.y);
+    min.x=point.x<min.x ? point.x : (pi == 0 ? point.x : min.x);
+    min.y=point.y<min.y ? point.y : (pi == 0 ? point.y : min.y);
+    max.x=point.x>max.x ? point.x : (pi == 0 ? point.x : max.x);
+    max.y=point.y>max.y ? point.y : (pi == 0 ? point.y : max.y);
   });
 
   center.x = (min.x + max.x)/2;
