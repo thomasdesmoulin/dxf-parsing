e27cf4bf5e5a536576aca13404a33699274f02f3
Thomas Desmoulin <thomas.desmoulin@laposte.net>
2014-06-30 21:56:32 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
-var Parser    = require(__dirname + '/libs/parser');
+var Parser    = require(__dirname + '/libs/parser'),
+    Circle    = require(__dirname + '/libs/elements/circle'),
+    Point     = require(__dirname + '/libs/elements/point'),
+    Polygon   = require(__dirname + '/libs/element/polygon'),
+    text      = require(__dirname + '/libs/element/text');
 
 module.exports = { 
-	Parser : Parser
+	Parser  : Parser,
+    Circle  : Circle,
+    Point   : Point,
+    Polygon : Polygon,
+    text    : text
 };
