587f242048bbdae872ea21b9a98015532e8d3bb6
Thomas Desmoulin <thomas.desmoulin@laposte.net>
2014-07-01 21:00:26 +0200

    fix bug

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var Parser    = require(__dirname + '/libs/parser'),
     Circle    = require(__dirname + '/libs/elements/circle'),
     Point     = require(__dirname + '/libs/elements/point'),
-    Polygon   = require(__dirname + '/libs/element/polygon'),
-    Text      = require(__dirname + '/libs/element/text');
+    Polygon   = require(__dirname + '/libs/elements/polygon'),
+    Text      = require(__dirname + '/libs/elements/text');
 
 module.exports = { 
 	Parser  : Parser,
